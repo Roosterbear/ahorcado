ab2fb65cdd3be785e09dcf8f55fa7febc8daaa9d
Roosterbear <lfpereag@gmail.com>
2024-05-24 01:31:19 -0600

    testing algorithm

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ guess.innerHTML = word_position;
 
 function crazymize(totalOfWords){
 
-  let odd_ = (new Date().getMilliseconds())%2;
-  return odd_;
+  let bit = (new Date().getMilliseconds())%2;
+  let odd = totalOfWords%2;
+  let half = Math.floor(totalOfWords/2);
+  
+  if (bit){
+    var numeros = Array.from({length: half}, (_, index) => index + 0);
+  }else{
+    var numeros = Array.from({length: half+odd}, (_, index) => index + half);
+  }
+  numeros = numeros.sort(() => Math.random() - 0.5);
+  return numeros[0];
   //return Math.floor(Math.random()*2);
   //return Math.floor(Math.random()*totalOfWords);
 }
