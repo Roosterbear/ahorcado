d747d57b98c24b9f1403f7c14ffcd5a23a644702
Roosterbear <lfpereag@gmail.com>
2024-05-24 00:38:00 -0600

    new algorithm crazymize

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
- const totalOfWords = words.length;
- const guess = document.querySelector(".guess");
+const totalOfWords = words.length;
+const guess = document.querySelector(".guess");
+
+word_position = crazymize(totalOfWords);
+
+guess.innerHTML = word_position;
 
 
- guess.innerHTML = totalOfWords;
+function crazymize(totalOfWords){
+
+  let odd_ = (new Date().getMilliseconds())%2;
+  return odd_;
+  //return Math.floor(Math.random()*2);
+  //return Math.floor(Math.random()*totalOfWords);
+}
